5526b90d6d94fb420bce5bdf36207c0f2290f32b
JustAD3v <gloryjust@outlook.fr>
2025-08-18 18:35:10 +0200

    Created Collider class

---
+++ include/Collider.h
@@ -0,0 +1,18 @@
+#ifndef COLLIDER_H
+#define COLLIDER_H
+
+#include "Vector2D.h"
+
+class Collider {
+    public:
+
+    private:
+        Collider() {};
+        Vector2D* m_Target;
+        Vector2D m_Position;
+        
+
+};
+
+
+#endif
